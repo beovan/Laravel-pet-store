c2649d9371a4bab154a8562fb55f66adaea89885
beovan <85023866+beovan@users.noreply.github.com>
2024-01-18 15:01:26 +0700

    fix branket

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -9,30 +10,31 @@ class OrderController extends Controller
 
     public function index()
     {
-       
-        $orders = Order::all(); 
+
+        $orders = Order::all();
         return view('orders.index', [
             'title' => 'Đơn hàng',
-            'orders' => $orders]);
+            'orders' => $orders
+        ]);
     }
     public function show($id)
     {
-       
+
         $order = Order::find($id);
         $user = auth()->user();
-     
+
         if (!$order) {
             abort(404);
 
-        // Retrieve the order items associated with the order
-        $orderItems = $order->items; // Assuming you have a relationship defined in your Order model
-        $orders = Order::where('customer_id', $user->id)->orderByDesc('created_at')->get();
-        return view('orders.show', [
-            'title' => 'Chi tiết đơn hàng',
-            'order' => $order,
-            'orders' => $orders,
-            'orderItems' => $orderItems
-        ]);
+            // Retrieve the order items associated with the order
+            $orderItems = $order->items; // Assuming you have a relationship defined in your Order model
+            $orders = Order::where('customer_id', $user->id)->orderByDesc('created_at')->get();
+            return view('orders.show', [
+                'title' => 'Chi tiết đơn hàng',
+                'order' => $order,
+                'orders' => $orders,
+                'orderItems' => $orderItems
+            ]);
+        }
     }
 }
-
