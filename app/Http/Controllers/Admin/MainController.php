639264d2e928bd369eaa5fcbb9fc226ad6356ee9
beovan <truongvangiap04@gmail.com>
2024-02-20 13:37:08 +0700

    perf: Refactor MainController and order.blade.php

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -7,23 +7,26 @@ use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\User as User;
 use App\Models\Product;
-
+use Carbon\Carbon;
 
 class MainController extends Controller
 {
     public function index()
     {
         $users = User::all();
-        $order = Order::all();
+        $orders = Order::all();
+        $sevenDaysAgo = Carbon::now()->subDays(7);
+        $recentOrders = $orders->where('created_at', '>=', $sevenDaysAgo);
         $products = Product::all();
-        echo view('admin.home',[
+        echo view('admin.home', [
             'title' => 'Trang Quản trị Admin',
-            "order" => $order,
+            "orders" => $recentOrders,
             "users" => $users,
             "products" => $products
         ]);
     }
-    function getSalesData() {
+    function getSalesData()
+    {
         // get data from database
         $data = Order::all();
 
--- resources/views/profile/order.blade.php
+++ resources/views/profile/order.blade.php
@@ -10,41 +10,82 @@
 
                 <div>
                     <i class="fas fa-envelope bg-primary"></i>
-                    <div class="timeline-item">
-                        <span class="time"><i class="far fa-clock"></i> {{ $order->created_at ? $order->created_at->format('H:i') : 'N/A' }}</span>
-                        <h3 class="timeline-header"><a href="#">Thông tin đơn hàng</a></h3>
-                        <div class="timeline-body">
-                            <!-- Display order-related information here -->
-                            <ul>
-                                <li> Tên: {{ $order->customer->name }}</li>
-                                <li> Trạng thái đơn hàng 
-                                    <div class="timeline-footer">
-                                        <a href="#" class="btn btn-primary btn-sm">
-                                            @if ($order->status === 'cancelled')
-                                                <p>Huỷ đơn hàng</p>
-                                            @elseif($order->status === 'processing')
-                                                <p>Bắt đầu giao hàng</p>
-                                            @elseif($order->status === 'delivered')
-                                                <p> Đã được giao hàng</p>
-                                            @elseif ($order->status === 'pending')
-                                                <p>Chờ xác nhận đơn hàng</p>
-                                            @else
-                                                <p>Unknown</p>
-                                            @endif
-                                        </a>
-                                    </div>
-                                </li>
-                            </ul>
-                            <!-- Add more order information here -->
+                    <div class="card">
+                        <div class="card-header border-transparent">
+                            <h3 class="card-title">Thông tin đơn hàng</h3>
+                            {{-- <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div> --}}
                         </div>
-                      
+                        <!-- /.card-header -->
+                        <div class="card-body p-0">
+                            <div class="table-responsive">
+                                <table class="table m-0">
+                                    <thead>
+                                        <tr>
+                                            <th>Mã số đơn hàng</th>
+                                            <th>Họ và tên</th>
+                                            <th>Trạng thái đơn hàng</th>
+                                            <th>Tổng đơn hàng</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><a>{{ $order->order_number }}</a></td>
+                                            <td><a>{{ $order->customer->name }}</a></td>
+                                            <td>
+                                                @if ($order->status === 'cancelled')
+                                                    <span class="badge badge-danger">
+                                                        <p>Huỷ đơn hàng</p>
+                                                    </span>
+                                                @elseif($order->status === 'processing')
+                                                    <span class="badge badge-info">
+                                                        <p>Bắt đầu giao hàng</p>
+                                                    </span>
+                                                @elseif($order->status === 'delivered')
+                                                    <span class="badge badge-success">
+                                                        <p>Đã được giao hàng</p>
+                                                    </span>
+                                                @elseif ($order->status === 'pending')
+                                                    <span class="badge badge-warning">
+                                                        <p>Chờ xác nhận đơn hàng</p>
+                                                    </span>
+                                                @else
+                                                    <span class="badge badge-secondary">
+                                                        <p>Unknown</p>
+                                                    </span>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                    {{ $order->total_amount }} VNĐ
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.table-responsive -->
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer clearfix">
+                            <a href="javascript:void(0)" class="btn btn-sm btn-secondary float-right">View All
+                                Orders</a>
+                        </div>
+                        <!-- /.card-footer -->
                     </div>
+                    <!-- /.card -->
                 </div>
             @endif
         @endforeach
         <div>
             <i class="fas fa-clock bg-gray">
-                {{$orders->links()}}
+                {{ $orders->links() }}
             </i>
         </div>
     </div>
